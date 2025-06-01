19373d624a257581f3f2808e6907187c3cf7342f
Oru Ovie <oruovie@gmail.com>
2025-06-01 06:57:43 +0100

    Fix site map issue

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export const baseURL = "https://kingoruovie.is-a.dev";
 export default async function sitemap() {
   const logs = getAllPosts().map((log) => ({
     url: `${baseURL}/logs/${log.slug}`,
-    lastModified: log.frontmatter.date.split("T")[0],
+    lastModified: new Date(log.frontmatter.date).toISOString().split("T")[0],
   }));
 
   const routes = ["", "/builds", "/about", "/logs"].map((route) => ({
