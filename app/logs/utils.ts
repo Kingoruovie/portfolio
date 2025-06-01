20f5d6114e6d639c8fc142a5f0c5c326eac1cc8f
Oru Ovie <oruovie@gmail.com>
2025-06-01 06:41:34 +0100

    Remove any

--- app/logs/utils.ts
+++ app/logs/utils.ts
@@ -2,11 +2,10 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 
-
 interface LogInterface {
   slug: string;
-  frontmatter: {[key: string]: any},
-  content: string
+  frontmatter: { [key: string]: string };
+  content: string;
 }
 
 const logsDirectory = path.join(process.cwd(), "logs");
@@ -30,7 +29,9 @@ export function getAllPosts(): LogInterface[] {
   });
 
   // Sort posts by date (newest first)
-  return posts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+  return posts.sort(
+    (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date),
+  );
 }
 
 // Get single post by slug
@@ -46,11 +47,15 @@ export function getPostBySlug(slug: string): LogInterface {
   };
 }
 
-export const logTag: {[key: string]: string} = {
+export const logTag: { [key: string]: string } = {
   INFO: "bg-blue-500 p-1 rounded-sm text-[12px] font-bold min-w-[50px] text-center",
-  DEBUG: "bg-indigo-500 p-1 rounded-sm text-[12px] font-bold min-w-[50px] text-center",
+  DEBUG:
+    "bg-indigo-500 p-1 rounded-sm text-[12px] font-bold min-w-[50px] text-center",
   WARN: "bg-yellow-500 p-1 rounded-sm text-[12px] font-bold min-w-[50px] text-center",
-  ERROR: "bg-red-500 p-1 rounded-sm text-[12px] font-bold min-w-[50px] text-center",
-  TRACE:  "bg-teal-500 p-1 rounded-sm text-[12px] font-bold min-w-[50px] text-center",
-  FATAL: "bg-blue-700 p-1 rounded-sm text-[12px] font-bold min-w-[50px] text-center"
-}
+  ERROR:
+    "bg-red-500 p-1 rounded-sm text-[12px] font-bold min-w-[50px] text-center",
+  TRACE:
+    "bg-teal-500 p-1 rounded-sm text-[12px] font-bold min-w-[50px] text-center",
+  FATAL:
+    "bg-blue-700 p-1 rounded-sm text-[12px] font-bold min-w-[50px] text-center",
+};
